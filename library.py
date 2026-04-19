57e937901b853322b8fae8a49fcb448189e2f5d6
Sabuhi <sabuhi.nazarov10@gmail.com>
2026-04-19 20:20:23 +0400

    initial commit

---
+++ library.py
@@ -0,0 +1,10 @@
+app_name = 'library'
+version = '0.1.0'
+is_active = True
+book_count = 0
+
+print(f"{app_name},{version}")
+
+def create_book(title, year, author, genre='Unknown'):
+    book = {"title": title, "year": year, "author": author, "genre": genre, "is_read": False}
+    return book
