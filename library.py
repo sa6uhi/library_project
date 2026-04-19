9c3719b0790d29e773d05230746e74885af1b0cc
Sabuhi <sabuhi.nazarov10@gmail.com>
2026-04-19 20:48:49 +0400

    add save and load library functions

--- library.py
+++ library.py
@@ -42,3 +42,15 @@ def book_iterator(genre_filter=None):
     for book in library:
         if genre_filter is None or book["genre"] == genre_filter:
             yield book
+
+import json
+
+def save_library(filename="file.json"):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(library, file, indent=2)
+
+try:
+    with open("file.json", "r", encoding="utf-8") as file:
+        library = json.load(file)
+except FileNotFoundError:
+    print("No existing library found, starting with an empty library.")
