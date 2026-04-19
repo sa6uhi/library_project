bff652a4a204f172cb746999924fff40773eaa7b
Sabuhi <sabuhi.nazarov10@gmail.com>
2026-04-19 20:21:51 +0400

    add is_calssic function

--- library.py
+++ library.py
@@ -8,3 +8,6 @@ print(f"{app_name},{version}")
 def create_book(title, year, author, genre='Unknown'):
     book = {"title": title, "year": year, "author": author, "genre": genre, "is_read": False}
     return book
+
+def is_classic(book):
+    return book["year"] < 1950
